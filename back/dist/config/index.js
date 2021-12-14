1d3e25147b75ac819b0a611f089e0cf27db20aeb
iiiiiii00iiiiiii <iiiiiii0.0iiiiiii@yahoo.com>
2021-12-14 21:32:41 +0900

    golden-0.0.0

--- back/dist/config/index.js
+++ back/dist/config/index.js
@@ -1,50 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// // GOLD
-// const config: TConfig = {
-//     db: {
-//         host: '18.183.255.153',
-//         port: 36183,
-//         name: 'gold',
-//         id: 'gold',
-//         password: 'Qufdl@4860'
-//     },
-//     casino: {
-//         apiURL: '',
-//         operator: '',
-//         privateKey: ''
-//     },
-//     // transactionOptions: {
-//     //     readPreference: 'primary',
-//     //     readConcern: { level: 'local' },
-//     //     writeConcern: { w: 'majority' }
-//     // },
-//     jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
-//     expiresIn: 60 * 60 * 1,
-//     pageSize: 30,
-//     sportPageSize: 30,
-//     sportsMaxRate: 200,
-//     powerballTime: 30,
-//     powerladderTime: 50,
-//     kenoladderTime: 50,
-//     boglePowerballTime: 30,
-//     bogleladderTime: 30,
-//     googlePowerball1: 15,
-//     googlePowerball3: 30,
-//     kplay: {
-//         code: 'SFT0001',
-//         token: 'cVsAlijIaYssmkF5qATyhPfoLRffQ9rR',
-//         key: 'IVSvfG2FtRdHqmdNciEZGYVzOiCP0GBB',
-//         endpoint: 'http://kplayone.com'
-//     }
-// }
 // GOLD
 const config = {
     db: {
         host: '18.183.255.153',
         port: 36183,
-        name: 'meta',
-        id: 'meta',
+        name: 'gold',
+        id: 'gold',
         password: 'Qufdl@4860'
     },
     casino: {
@@ -52,6 +14,11 @@ const config = {
         operator: '',
         privateKey: ''
     },
+    // transactionOptions: {
+    //     readPreference: 'primary',
+    //     readConcern: { level: 'local' },
+    //     writeConcern: { w: 'majority' }
+    // },
     jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
     expiresIn: 60 * 60 * 1,
     pageSize: 30,
@@ -65,11 +32,44 @@ const config = {
     googlePowerball1: 15,
     googlePowerball3: 30,
     kplay: {
-        code: 'SFT0002',
-        token: 'imWlwA8BvfhZpCmas6x42Ow0itUGIV7j',
-        key: 'Jgl6qopQoaDUD0HUq9YyiIdLif7tDR0i',
+        code: '',
+        token: '',
+        key: '',
         endpoint: 'http://kplayone.com'
-        // http://api.kplay.jmt.soft24.io/api
     }
 };
+// // GOLD
+// const config: TConfig = {
+//     db: {
+//         host: '18.183.255.153',
+//         port: 36183,
+//         name: 'meta',
+//         id: 'meta',
+//         password: 'Qufdl@4860'
+//     },
+//     casino: {
+//         apiURL: '',
+//         operator: '',
+//         privateKey: ''
+//     },
+//     jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
+//     expiresIn: 60 * 60 * 1,
+//     pageSize: 30,
+//     sportPageSize: 30,
+//     sportsMaxRate: 200,
+//     powerballTime: 30,
+//     powerladderTime: 50,
+//     kenoladderTime: 50,
+//     boglePowerballTime: 30,
+//     bogleladderTime: 30,
+//     googlePowerball1: 15,
+//     googlePowerball3: 30,
+//     kplay: {
+//         code: 'SFT0002',
+//         token: 'imWlwA8BvfhZpCmas6x42Ow0itUGIV7j',
+//         key: 'Jgl6qopQoaDUD0HUq9YyiIdLif7tDR0i',
+//         endpoint: 'http://kplayone.com'
+//         // http://api.kplay.jmt.soft24.io/api
+//     }
+// }
 exports.default = config;
