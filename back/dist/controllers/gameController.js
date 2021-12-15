12136c55af86e3c8d98b437a5c7f1368dd1d5e0a
iiiiiii00iiiiiii <iiiiiii0.0iiiiiii@yahoo.com>
2021-12-15 22:45:35 +0900

    golden-0.0.0

--- back/dist/controllers/gameController.js
+++ back/dist/controllers/gameController.js
@@ -705,13 +705,63 @@ class GameController {
                     return;
                 }
                 // ■■■■■■■■■■ DB-스포츠 경기 리스트 가져오기 ■■■■■■■■■■
+                // for(let i: number = 0; i < r.data.length; i++) {
+                //     if(r.data[i].games.handicap) {
+                //         r.data[i].games.handicap = _.sortBy(r.data[i].games.handicap, 'homeStandard')
+                //     }
+                //     if(r.data[i].games.underOver) {
+                //         r.data[i].games.underOver = _.sortBy(r.data[i].games.underOver, 'standard')
+                //     }
+                // }
                 for (let i = 0; i < r.data.length; i++) {
                     if (r.data[i].games.handicap) {
-                        r.data[i].games.handicap = modules_1._.sortBy(r.data[i].games.handicap, 'homeStandard');
+                        let h = 1000000;
+                        let indexH = -1;
+                        for (let j = 0; j < r.data[i].games.handicap.length; j++) {
+                            let nh = Math.abs(r.data[i].games.handicap[j].homeRate - r.data[i].games.handicap[j].awayRate);
+                            if (nh <= h) {
+                                h = nh;
+                                indexH = j;
+                            }
+                        }
+                        r.data[i].games.handicap = [r.data[i].games.handicap[indexH]];
                     }
+                    // if(r.data[i].games.handicapTotalSet) {
+                    //     let h = 1000000
+                    //     let indexH = -1
+                    //     for(let j = 0; j < r.data[i].games.handicapTotalSet.length; j++) {
+                    //         let nh = Math.abs(r.data[i].games.handicapTotalSet[j].homeRate - r.data[i].games.handicapTotalSet[j].awayRate)
+                    //         if(nh < h) {
+                    //             h = nh
+                    //             indexH = j
+                    //         }
+                    //     }
+                    //     r.data[i].games.handicapTotalSet = [r.data[i].games.handicapTotalSet[indexH]]
+                    // }
                     if (r.data[i].games.underOver) {
-                        r.data[i].games.underOver = modules_1._.sortBy(r.data[i].games.underOver, 'standard');
+                        let h = 1000000;
+                        let indexH = -1;
+                        for (let j = 0; j < r.data[i].games.underOver.length; j++) {
+                            let nh = Math.abs(r.data[i].games.underOver[j].underRate - r.data[i].games.underOver[j].overRate);
+                            if (nh <= h) {
+                                h = nh;
+                                indexH = j;
+                            }
+                        }
+                        r.data[i].games.underOver = [r.data[i].games.underOver[indexH]];
                     }
+                    // if(r.data[i].games.underOverTotalSet) {
+                    //     let h = 1000000
+                    //     let indexH = -1
+                    //     for(let j = 0; j < r.data[i].games.underOverTotalSet.length; j++) {
+                    //         let nh = Math.abs(r.data[i].games.underOverTotalSet[j].underRate - r.data[i].games.underOverTotalSet[j].overRate)
+                    //         if(nh < h) {
+                    //             h = nh
+                    //             indexH = j
+                    //         }
+                    //     }
+                    //     r.data[i].games.underOverTotalSet = [r.data[i].games.underOverTotalSet[indexH]]
+                    // }
                 }
                 // ■■■■■■■■■■ DB-스포츠 환경 설정 가져오기 ■■■■■■■■■■
                 const getKeys = ['lv1', 'lv2', 'lv3', 'lv4', 'lv5', 'lv6', 'lv7', 'lv8', 'lv9'];
