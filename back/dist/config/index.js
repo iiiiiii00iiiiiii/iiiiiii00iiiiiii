b6bad016d845ef067559d23f2fd189d11cf4dd3f
iiiiiii00iiiiiii <iiiiiii0.0iiiiiii@yahoo.com>
2022-02-15 02:11:16 +0900

    0.0.0

--- back/dist/config/index.js
+++ back/dist/config/index.js
@@ -38,40 +38,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //         endpoint: 'http://kplayone.com'
 //     }
 // }
-// // META
-// const config: TConfig = {
-//     db: {
-//         host: '18.183.255.153',
-//         port: 36183,
-//         name: 'meta',
-//         id: 'meta',
-//         password: 'Qufdl@4860'
-//     },
-//     casino: {
-//         apiURL: '',
-//         operator: '',
-//         privateKey: ''
-//     },
-//     jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
-//     expiresIn: 60 * 60 * 1,
-//     pageSize: 30,
-//     sportPageSize: 30,
-//     sportsMaxRate: 200,
-//     powerballTime: 30,
-//     powerladderTime: 50,
-//     kenoladderTime: 50,
-//     boglePowerballTime: 30,
-//     bogleladderTime: 30,
-//     googlePowerball1: 15,
-//     googlePowerball3: 30,
-//     kplay: {
-//         code: 'SFT0002',
-//         token: 'imWlwA8BvfhZpCmas6x42Ow0itUGIV7j',
-//         key: 'Jgl6qopQoaDUD0HUq9YyiIdLif7tDR0i',
-//         endpoint: 'http://kplayone.com'
-//         // http://api.kplay.jmt.soft24.io/api
-//     }
-// }
+// META
+const config = {
+    db: {
+        host: '18.183.255.153',
+        port: 36183,
+        name: 'meta',
+        id: 'meta',
+        password: 'Qufdl@4860'
+    },
+    casino: {
+        apiURL: '',
+        operator: '',
+        privateKey: ''
+    },
+    jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
+    expiresIn: 60 * 60 * 1,
+    pageSize: 30,
+    sportPageSize: 30,
+    sportsMaxRate: 200,
+    powerballTime: 30,
+    powerladderTime: 50,
+    kenoladderTime: 50,
+    boglePowerballTime: 30,
+    bogleladderTime: 30,
+    googlePowerball1: 15,
+    googlePowerball3: 30,
+    kplay: {
+        code: 'SFT0002',
+        token: 'imWlwA8BvfhZpCmas6x42Ow0itUGIV7j',
+        key: 'Jgl6qopQoaDUD0HUq9YyiIdLif7tDR0i',
+        endpoint: 'http://kplayone.com'
+        // http://api.kplay.jmt.soft24.io/api
+    }
+};
 // // THREE
 // const config: TConfig = {
 //     db: {
@@ -174,38 +174,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //         // http://api.kplay.jmt.soft24.io/api
 //     }
 // }
-// BACKNUM
-const config = {
-    db: {
-        host: '18.183.255.153',
-        port: 36183,
-        name: 'backnum',
-        id: 'backnum',
-        password: 'Qufdl@4860'
-    },
-    casino: {
-        apiURL: '',
-        operator: '',
-        privateKey: ''
-    },
-    jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
-    expiresIn: 60 * 60 * 1,
-    pageSize: 30,
-    sportPageSize: 30,
-    sportsMaxRate: 200,
-    powerballTime: 30,
-    powerladderTime: 50,
-    kenoladderTime: 50,
-    boglePowerballTime: 30,
-    bogleladderTime: 30,
-    googlePowerball1: 15,
-    googlePowerball3: 30,
-    kplay: {
-        code: '',
-        token: '',
-        key: '',
-        endpoint: 'http://kplayone.com'
-        // http://api.kplay.jmt.soft24.io/api
-    }
-};
+// // BACKNUM
+// const config: TConfig = {
+//     db: {
+//         host: '18.183.255.153',
+//         port: 36183,
+//         name: 'backnum',
+//         id: 'backnum',
+//         password: 'Qufdl@4860'
+//     },
+//     casino: {
+//         apiURL: '',
+//         operator: '',
+//         privateKey: ''
+//     },
+//     jwtSecret: 'To!@#$%^&*()0987654321Esta365!To',
+//     expiresIn: 60 * 60 * 1,
+//     pageSize: 30,
+//     sportPageSize: 30,
+//     sportsMaxRate: 200,
+//     powerballTime: 30,
+//     powerladderTime: 50,
+//     kenoladderTime: 50,
+//     boglePowerballTime: 30,
+//     bogleladderTime: 30,
+//     googlePowerball1: 15,
+//     googlePowerball3: 30,
+//     kplay: {
+//         code: '',
+//         token: '',
+//         key: '',
+//         endpoint: 'http://kplayone.com'
+//         // http://api.kplay.jmt.soft24.io/api
+//     }
+// }
 exports.default = config;
