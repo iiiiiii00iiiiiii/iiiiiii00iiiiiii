1fca17a0d498277dc765b4b8d13aa38de3457934
iiiiiii00iiiiiii <iiiiiii0.0iiiiiii@yahoo.com>
2022-04-28 18:34:37 +0900

    0.0.0

--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -22,29 +22,29 @@
 //     versionKey: 'version'
 // }
 
-// // META
-// module.exports = {
-//     apps: [
-//         {
-//             name: "[META]CLIENT",
-//             script: "./dist/app.js",
-//             env: {
-//                 "PORT": 2011,
-//                 "NODE_ENV": "development"
-//             },
-//             env_production: {
-//                 "PORT": 2011,
-//                 "NODE_ENV": "production"
-//             }
-//         }
-//     ],
-//     frontPort: 2010,
-//     port: 2011,
-//     pageSize: 30,
-//     pageLimit: 7,
-//     version: '0.0.0',
-//     versionKey: 'version'
-// }
+// META
+module.exports = {
+    apps: [
+        {
+            name: "[META]CLIENT",
+            script: "./dist/app.js",
+            env: {
+                "PORT": 2011,
+                "NODE_ENV": "development"
+            },
+            env_production: {
+                "PORT": 2011,
+                "NODE_ENV": "production"
+            }
+        }
+    ],
+    frontPort: 2010,
+    port: 2011,
+    pageSize: 30,
+    pageLimit: 7,
+    version: '0.0.0',
+    versionKey: 'version'
+}
 
 // // THREE
 // module.exports = {
@@ -166,29 +166,29 @@
 //     versionKey: 'version'
 // }
 
-// STUDY
-module.exports = {
-    apps: [
-        {
-            name: "[STUDY]CLIENT",
-            script: "./dist/app.js",
-            env: {
-                "PORT": 2007,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 2007,
-                "NODE_ENV": "production"
-            }
-        }
-    ],
-    frontPort: 2006,
-    port: 2007,
-    pageSize: 30,
-    pageLimit: 7,
-    version: '0.0.0',
-    versionKey: 'version'
-}
+// // STUDY
+// module.exports = {
+//     apps: [
+//         {
+//             name: "[STUDY]CLIENT",
+//             script: "./dist/app.js",
+//             env: {
+//                 "PORT": 2007,
+//                 "NODE_ENV": "development"
+//             },
+//             env_production: {
+//                 "PORT": 2007,
+//                 "NODE_ENV": "production"
+//             }
+//         }
+//     ],
+//     frontPort: 2006,
+//     port: 2007,
+//     pageSize: 30,
+//     pageLimit: 7,
+//     version: '0.0.0',
+//     versionKey: 'version'
+// }
 
 // // PENT
 // module.exports = {
